835ad8ece3bfe03cfe1cb6a177fc7653d9930e17
Jesus Conde <educaleft@gmail.com>
2021-12-05 19:52:40 +0000

    Nueva versión del programa con NOT, ahora correcta.

--- Logicos/Logicos.cpp
+++ Logicos/Logicos.cpp
@@ -5,13 +5,38 @@ int main()
 	int x{ 5 };
 	int y{ 7 };
 
+	if (!(x > y))
+		std::cout << x << " no es mayor que " << y << '\n';
+	else
+		std::cout << x << " es mayor que " << y << '\n';
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+int main()
+{
+	int x{ 5 };
+	int y{ 7 };
+
 	if (!x > y)
 		std::cout << x << " no es mayor que  " << y << '\n';
 	else
 		std::cout << x << " es mayor que " << y << '\n';
 
 	return 0;
-}
+}*/
 
 
 
