c080b16ff4f2d5b089bc48d710056a8fc35b943a
Jesus Conde <educaleft@gmail.com>
2021-12-05 19:47:06 +0000

    Cambiados strings

--- Logicos/Logicos.cpp
+++ Logicos/Logicos.cpp
@@ -6,9 +6,9 @@ int main()
 	int y{ 7 };
 
 	if (!x > y)
-		std::cout << x << " is not greater than " << y << '\n';
+		std::cout << x << " no es mayor que  " << y << '\n';
 	else
-		std::cout << x << " is greater than " << y << '\n';
+		std::cout << x << " es mayor que " << y << '\n';
 
 	return 0;
 }
